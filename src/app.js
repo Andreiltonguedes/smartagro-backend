814fba75f14f19c7e7ab0fe961329be658a3985b
Andreiltonguedes <Andreilton.guedes@gmail.com>
2025-08-02 17:32:21 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const userRoutes = require('./routes/usuarios');
+const sensorRoutes = require('./routes/sensores');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/usuarios', userRoutes);
+app.use('/sensores', sensorRoutes);
+
+module.exports = app;
